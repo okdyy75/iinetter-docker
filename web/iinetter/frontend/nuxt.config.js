8338f7eb66467cfa8732f3526c2d2d838c822b24
okdyy75 <okdyy75@gmail.com>
2021-01-02 08:39:46 +0900

    googleアナリティクス設定

--- web/iinetter/frontend/nuxt.config.js
+++ web/iinetter/frontend/nuxt.config.js
@@ -35,6 +35,7 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
+    { src: '~plugins/ga.js', mode: 'client' }
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
