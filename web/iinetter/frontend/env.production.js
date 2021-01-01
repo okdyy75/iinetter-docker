c6bfe2b9c8d43ad286cf6884d0db2a1b22d04f16
okdyy75 <okdyy75@gmail.com>
2021-01-02 02:52:09 +0900

    Heroku向けfrontendデプロイを設定

---
+++ web/iinetter/frontend/env.production.js
@@ -0,0 +1,7 @@
+module.exports = {
+  NODE_ENV: process.env.NODE_ENV,
+  BASE_URL: 'https://iinetter.herokuapp.com',
+  API_BASE_URL: 'https://adm-iinetter.herokuapp.com/',
+  DEFAULT_TITLE: 'IInetter',
+  DEFAULT_DESCRIPTION: 'IInetter(いいねがいっぱい出来るtwitter風SNS)'
+}
--- web/iinetter/frontend/nuxt.config.js
+++ web/iinetter/frontend/nuxt.config.js
@@ -3,10 +3,10 @@ const env = require(`./env.${environment}.js`)
 
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
-  ssr: false,
+  ssr: true,
 
   // Target (https://go.nuxtjs.dev/config-target)
-  target: 'static',
+  target: 'server',
 
   env: {
     ...env
@@ -65,6 +65,10 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    babel: {
+      babelrc: false,
+      compact: false
+    }
   },
   auth: {
     redirect: {
