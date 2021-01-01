834f0e0a65256db0e129363157d678620236d889
okdyy75 <okdyy75@gmail.com>
2021-01-02 05:06:58 +0900

    Heroku向けbackendデプロイを設定

--- web/iinetter/app/Providers/AppServiceProvider.php
+++ web/iinetter/app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 
     /**
--- web/iinetter/database/seeders/DatabaseSeeder.php
+++ web/iinetter/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'is_admin' => 1,
+        ]);
     }
 }
