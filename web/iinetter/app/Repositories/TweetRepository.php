f7d8b5eb2140cd126c5dfeaf29c17454deeb95d1
okdyy75 <okdyy75@gmail.com>
2021-01-08 07:43:26 +0900

    reply_countとretweet_countが更新されるように修正

--- web/iinetter/app/Repositories/TweetRepository.php
+++ web/iinetter/app/Repositories/TweetRepository.php
@@ -40,6 +40,35 @@ class TweetRepository extends BaseRepository
     }
 
     /**
+     * Create model record
+     *
+     * @param array $input
+     *
+     * @return Model
+     */
+    public function create($input)
+    {
+        $model = $this->model->newInstance($input);
+
+        $model->save();
+
+        // ツイート・リツイートのカウント更新
+        if ($input['tweet_type'] === 'reply') {
+            $refTweet = $this->find($input['ref_tweet_id']);
+            if (! empty($refTweet)) {
+                $refTweet->increment('reply_count');
+            }
+        } else if ($input['tweet_type'] === 'retweet') {
+            $refTweet = $this->find($input['ref_tweet_id']);
+            if (! empty($refTweet)) {
+                $refTweet->increment('retweet_count');
+            }
+        }
+
+        return $model;
+    }
+
+    /**
      * ユーザーIDからtweet検索
      *
      * @return Collection
