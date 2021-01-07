ad55a51dc7e735b63247908e0488b743bd2387cb
okdyy75 <okdyy75@gmail.com>
2021-01-08 07:09:14 +0900

    resoucesの日本語対応

--- web/iinetter/resources/lang/ja/auth.php
+++ web/iinetter/resources/lang/ja/auth.php
@@ -14,6 +14,59 @@ return [
     */
 
     'failed' => 'ログイン情報が登録されていません。',
+    'password' => '入力されたパスワードが正しくありません。',
     'throttle' => 'ログインに続けて失敗しています。:seconds秒後に再度お試しください。',
 
+    'full_name'        => '氏名',
+    'email'            => 'メールアドレス',
+    'password'         => 'パスワード',
+    'confirm_password' => 'パスワード確認',
+    'remember_me'      => 'Remember Me',
+    'sign_in'          => 'ログイン',
+    'sign_out'         => 'ログアウト',
+    'register'         => '登録',
+
+    'login' => [
+        'title'               => 'ログイン',
+        'forgot_password'     => 'パスワードを忘れた',
+        'register_membership' => '新規登録',
+    ],
+
+    'registration' => [
+        'title'           => '新規登録',
+        'i_agree'         => '同意する',
+        'terms'           => '規約',
+        'have_membership' => '既に登録済みです',
+    ],
+
+    'forgot_password' => [
+        'title'          => 'パスワードを初期化するには、Eメールを入力してください',
+        'send_pwd_reset' => 'メール送信',
+    ],
+
+    'reset_password' => [
+        'title'         => 'パスワードをリセット',
+        'reset_pwd_btn' => 'パスワードリセット',
+    ],
+
+    'emails' => [
+        'password' => [
+            'reset_link' => 'クリックしてパスワードをリセット',
+        ],
+    ],
+
+    'app' => [
+        'member_since' => 'Member since',
+        'messages'     => 'Messages',
+        'settings'     => 'Settings',
+        'lock_account' => 'Lock Account',
+        'profile'      => 'Profile',
+        'online'       => 'Online',
+        'search'       => 'Search',
+        'create'       => 'Create',
+        'export'       => 'Export',
+        'print'        => 'Print',
+        'reset'        => 'Reset',
+        'reload'       => 'Reload',
+    ],
 ];
